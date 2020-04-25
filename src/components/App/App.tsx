2c7c2c330a69b11b7c7a143c1c0542bc7f82cb35
Łukasz Wójcik <lwojcik@lukem.net>
2020-04-25 02:16:08 +0200

    remove console.log lol

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -9,22 +9,19 @@ import WidgetPage from 'components/WidgetPage/WidgetPage';
 import IndexPage from 'components/IndexPage/IndexPage';
 import { widgets } from 'config/config';
 
-const App = () => {
-  console.log(widgets);
-  return (
-    <Router>
-      <Switch>
-        {widgets.map((widget, key) => (
-          <Route key={key} path={widget.route}>
-            <WidgetPage widget={widget} />
-          </Route>
-        ))}
-        <Route path="/">
-          <IndexPage widgets={widgets} />
+const App = () => (
+  <Router>
+    <Switch>
+      {widgets.map((widget, key) => (
+        <Route key={key} path={widget.route}>
+          <WidgetPage widget={widget} />
         </Route>
-      </Switch>
-    </Router>
-  );
-}
+      ))}
+      <Route path="/">
+        <IndexPage widgets={widgets} />
+      </Route>
+    </Switch>
+  </Router>
+);
 
 export default App;
