7e11aab1f66d988b90e51ab121ad0be1cba40cf8
Łukasz Wójcik <lwojcik@lukem.net>
2020-04-30 23:15:33 +0200

    more test coverage

---
+++ src/components/WidgetPreview/WidgetPreview.test.tsx
@@ -0,0 +1,15 @@
+import WidgetPreview from './WidgetPreview'
+import domRender from 'utils/domRender';
+
+const testParams = {
+  alias: 'test',
+  params: {
+    h: 1,
+    m: 2,
+    s: 3,
+  }
+}
+
+it('renders correctly', () => {
+  domRender(WidgetPreview, testParams);
+});
--- src/helpers/constructURLparams/constructURLparams.ts
+++ src/helpers/constructURLparams/constructURLparams.ts
@@ -1,7 +1,7 @@
 import { ShorthandValues } from 'types';
 
 const constructURLparams = (params: ShorthandValues) =>
-params && Object.keys(params).length > 0
+  params && Object.keys(params).length > 0
     ? `?${Object.keys(params).map(key =>
         `${key}=${encodeURIComponent(params[key])}`)
       .join('&')}`
