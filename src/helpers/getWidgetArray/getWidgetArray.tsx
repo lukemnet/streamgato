1d3c8f63bef00cc0a3a0558cc97f4f2a157e8ed1
Łukasz Wójcik <lwojcik@lukem.net>
2020-05-02 01:17:50 +0200

    remove unneeded function param type

--- src/helpers/getWidgetArray/getWidgetArray.tsx
+++ src/helpers/getWidgetArray/getWidgetArray.tsx
@@ -5,7 +5,7 @@ import { Widget } from 'types';
 type Element = (widget: Widget, key: string) =>
   ReactElement<{ widget: Widget, key: string }>;
 
-const getWidgetArray = (element?: Element | Element) => {
+const getWidgetArray = (element?: Element) => {
   if (element) {
     return widgets.map(widget => element(widget, widget.alias));
   }
