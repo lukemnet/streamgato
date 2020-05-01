ec855d02de45d6839f2410b94cbb56be0ee546a4
Łukasz Wójcik <lwojcik@lukem.net>
2020-05-01 23:17:15 +0200

    timers

--- src/components/StreamTimer/StreamTimer.tsx
+++ src/components/StreamTimer/StreamTimer.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import { WidgetComponentProps } from 'types';
+import Timer from 'react-compound-timer';
+import addLeadingZeros from 'helpers/addLeadingZeros/addLeadingZeros';
 
 const StreamTimer = ({ params }: WidgetComponentProps) => {
   console.log(params);
@@ -7,6 +9,19 @@ const StreamTimer = ({ params }: WidgetComponentProps) => {
     <div>
       <p>StreamTimer</p>
       <p>{JSON.stringify(params)}</p>
+      <Timer
+        lastUnit="h"
+        direction="forward"
+        formatValue={addLeadingZeros}
+      >
+        {() => (
+          <>
+            <Timer.Hours />:
+            <Timer.Minutes />:
+            <Timer.Seconds />
+          </>
+        )}
+      </Timer>
     </div>
   );
 }
