abb49c773abedc48e031a082d69787fe084e1bad
Łukasz Wójcik <lwojcik@lukem.net>
2020-05-03 13:04:32 +0200

    minor fixes

--- src/components/WidgetUrlSection/WidgetUrlSection.tsx
+++ src/components/WidgetUrlSection/WidgetUrlSection.tsx
@@ -1,5 +1,8 @@
-import React from 'react';
-import WidgetUrl from 'components/WidgetUrl/WidgetUrl';
+import React, { FocusEvent } from 'react';
+import { Button, InputGroup, FormControl } from 'react-bootstrap';
+// import WidgetUrl from 'components/WidgetUrl/WidgetUrl';
+import { FaCopy } from 'react-icons/fa';
+import getWidgetUrl from 'helpers/getWidgetUrl/getWidgetUrl';
 import { ShorthandValues } from 'types';
 
 interface WidgetUrlProps {
@@ -7,13 +10,47 @@ interface WidgetUrlProps {
   params: ShorthandValues;
 }
 
-const WidgetUrlSection = (props: WidgetUrlProps) => (
-  <div style={{ overflow: 'hidden' }}>
-    <h2>Widget URL</h2>
+const WidgetUrlSection = (props: WidgetUrlProps) => {
+  const { alias, params } = props;
+  const widgetUrl = getWidgetUrl(alias, params);
+  const emptyParams = params && Object.keys(params).length <= 0;
+  const onFocus =  (e: FocusEvent<HTMLInputElement>) => {
+    e.target.select();
+  };
+
+  return (
     <div>
-      <WidgetUrl {...props} />
+      <h2>Widget URL</h2>
+      <div>
+        <InputGroup className='mb-3'>
+          <FormControl
+            {...emptyParams && {
+              disabled: true,
+              title: 'Configure widget first',
+            }}
+            readOnly
+            value={widgetUrl}
+            onFocus={onFocus}
+            aria-label='Widget URL'
+          />
+          <InputGroup.Append>
+            <Button
+              {...emptyParams && {
+                disabled: true,
+                readOnly: true }}
+              title={emptyParams
+                ? 'Configure widget first'
+                : 'Copy widget URL'}
+              variant='outline-secondary'
+            >
+              <FaCopy />
+            </Button>
+          </InputGroup.Append>
+        </InputGroup>
+        {/* <WidgetUrl {...props} /> */}
+      </div>
     </div>
-  </div>
-);
+  );
+};
 
 export default WidgetUrlSection;
