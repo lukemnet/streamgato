35a9cc388fcb04a4ccc85f8299168cd09d337b25
Łukasz Wójcik <1711174+lwojcik@users.noreply.github.com>
2020-05-03 20:36:20 +0200

    Stage 1 (#10)
    * redirects
    
    * remove prod domain

--- src/components/WidgetPage/WidgetPage.tsx
+++ src/components/WidgetPage/WidgetPage.tsx
@@ -5,7 +5,6 @@ import StreamTimer from 'components/StreamTimer/StreamTimer';
 import SocialMediaWidget from 'components/SocialMediaWidget/SocialMediaWidget';
 import LastFmRecentSong from 'components/LastFmRecentSong/LastFmRecentSong';
 import { WidgetAlias, WidgetComponent } from 'types';
-import { metadata } from 'config/config';
 
 interface WidgetPageProps {
   alias: WidgetAlias;
@@ -20,7 +19,7 @@ const WidgetPage = ({ alias, configRoute }: WidgetPageProps) => {
   const params = getURLparams();
 
   if (Object.keys(params).length <= 0)
-    window.location.replace(`${metadata.prodOrigin}${configRoute}`);
+    window.location.replace(configRoute);
 
   const widgets = {
     ct: <CountdownTimer params={params} />,
